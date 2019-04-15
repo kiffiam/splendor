63442a9826ffc8ad4ad7ef8e90648be22a503976
Kalanovics Zoltán <kalanovics.z@gmail.com>
2019-04-15 16:25:45 +0200

    base

---
+++ Assets/Scripts/Card/CardStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardStats : MonoBehaviour
+{
+    public int redChipsValue;
+    public int greenChipsValue;
+    public int whiteChipsValue;
+    public int blueChipsValue;
+    public int blackChipsValue;
+
+    public Color color;
+
+    public int pointValue;
+
+    //void onClick(){} kattintásra vagy felveszi a kártyát vagy visszautasítja a játékost
+
+    //void OnRightClick(){} foglalás, arany check. kihelyezni a saját deckbe. left click ugyanúgy mukődik rá. 
+
+    //void onHover() ha a kurzor rajta van akkor kiírja az értékét
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ChipStash/ChipStashService.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChipStashService : MonoBehaviour
+{
+    public Color stashColor;
+
+    public int stashNumber;
+
+    //elvesz egyet a stashből
+    void OnLeftClick() { }
+
+    //elvesz kettőt a stashből, ha legalább kettő maradna a vétel után, következő játékos
+    void OnRigthClick()
+    {
+        if (stashNumber - 2 >= 2)
+        {
+            //itt átmegy a 2 chips a játékoshoz
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
