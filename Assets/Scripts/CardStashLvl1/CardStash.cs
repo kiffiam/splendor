47b9d3bcde0b57accad7012f4f5c8b6b29f9f280
Kalanovics Zoltán <kalanovics.z@gmail.com>
2019-05-06 19:02:27 +0200

    folder changes
    Initiated prefab read into card stashes, card stash script, chip prefabss

---
+++ Assets/Scripts/CardStashLvl1/CardStash.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardStash : MonoBehaviour
+{
+    public int cardNumber;
+    public List<GameObject> cards;
+    public string lvl;
+
+    private void Awake()
+    {
+        lvl = name.Substring(3, 1);
+        cards = new List<GameObject>(Resources.LoadAll<GameObject>("Prefabs/lvl" + lvl + "Prefabs"));
+        cardNumber = cards.Count;
+        
+        //animációahogy beesnek ebbe a cuccba vagy egymásra
+        //TODO
+
+        PlaceStartingCards();
+    }
+
+    //called in awake
+    public void PlaceStartingCards()
+    {
+        //intantiate meglévő koordinátákra
+    }
+
+    //called by cards which is being picked up
+    public void PlaceCard(Vector3 cardPlace)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
